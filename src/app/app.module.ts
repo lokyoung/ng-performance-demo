1a76ccacd7925c2a5933ce5ee63168b729739c81
lokyoung <lokyoung@outlook.com>
2018-05-28 22:50:05 +0800

    feat: init project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
+import { AppRoutingModule } from './app.routing';
+import { SidebarModule } from 'primeng/sidebar';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,7 +11,10 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    SidebarModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
